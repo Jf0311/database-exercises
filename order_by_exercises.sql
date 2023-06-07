bc0181819d3b2cecd14522a103ee188e5747e566
Joseph Fuentes <josephkfuentes@gmail.com>
2023-06-07 15:46:25 -0400

    Finished Second Part

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -12,4 +12,13 @@ SELECT * FROM employees WHERE (last_name LIKE 'E%') AND last_name LIKE '%E' ;
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ;
 
-SELECT first_name, last_name FROM employees WHERE  first_name = 'Irena' OR  first_name = 'Vidya' OR  first_name =  'Maya' ORDER BY first_name;
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name  NOT LIKE '%qu%';
+
+SELECT first_name, last_name FROM employees WHERE  first_name = 'Irena' OR  first_name = 'Vidya' OR  first_name =  'Maya' ORDER BY first_name, last_name;
+
+SELECT first_name, last_name FROM employees WHERE  first_name = 'Irena' OR  first_name = 'Vidya' OR  first_name =  'Maya' ORDER BY  last_name ASC ,first_name ASC;
+
+SELECT  last_name, emp_no FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no;
+
+SELECT  last_name, emp_no FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC ;
+
